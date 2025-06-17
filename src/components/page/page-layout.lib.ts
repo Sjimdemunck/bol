193578057656e9d6a3e145482f96ecd13a55b7d9
Sjim <sjim@codecapi.nl>
2025-06-17 23:22:35 +0200

    Chore: polish app ui and add in socials

--- src/components/page/page-layout.lib.ts
+++ src/components/page/page-layout.lib.ts
@@ -1,5 +1,6 @@
 import type { User } from '@/types';
 
+// Some default user data mostly used for basic app
 export const user = {
   name: 'SjimBolSlice',
   email: 'Sjim@bol.com',
@@ -12,4 +13,13 @@ export const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
+];
+export const UserSocial = [
+  { name: 'Email', href: 'mailto:sjim@codecapi.nl', icon: 'Mail' },
+  { name: 'GitHub', href: 'https://github.com/Sjimdemunck', icon: 'GitHub' },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/sjim-de-munck/',
+    icon: 'Linkedin',
+  },
 ];
--- src/components/page/page-layout.tsx
+++ src/components/page/page-layout.tsx
@@ -2,6 +2,14 @@ import Header from './header';
 import { Card, CardContent } from '@/components/ui/card';
 import Footer from './footer';
 import { CategoryFilter } from '../category/category-filter';
+import { Mail, Linkedin, Github } from 'lucide-react';
+import { UserSocial } from './page-layout.lib';
+
+export const iconMap = {
+  Mail,
+  GitHub: Github,
+  Linkedin,
+};
 
 export default function PageLayout() {
   return (
@@ -21,11 +29,17 @@ export default function PageLayout() {
                   <Card>
                     <CardContent>
                       <h2 className="text-2xl">Multiselect filter - Inline</h2>
+                      <span className="text-xs">
+                        ⚠️ Both variants share the same store*
+                        <br />
+                        The second variant is just for demonstration of data
+                        table filtering
+                      </span>
                       <CategoryFilter />
                     </CardContent>
                   </Card>
                 </section>
-                {/* <section aria-labelledby="section-1-title">
+                <section aria-labelledby="section-1-title">
                   <Card>
                     <CardContent>
                       <h2 className="text-2xl">Multiselect filter - Popover</h2>
@@ -35,17 +49,44 @@ export default function PageLayout() {
                       <CategoryFilter variant="popover" />
                     </CardContent>
                   </Card>
-                </section> */}
+                </section>
               </div>
 
               {/* Right column */}
               <div className="grid grid-cols-1 gap-4">
                 <section aria-labelledby="section-2-title">
                   <h2 id="section-2-title" className="sr-only">
-                    Product summary
+                    User info
                   </h2>
                   <Card>
-                    <CardContent>Product Summary or some sorts</CardContent>
+                    <CardContent>
+                      <h2 className="text-2xl mb-2">User Info</h2>
+                      <p className="text-sm text-muted-foreground mb-4">
+                        You can reach out via any of the platforms below.
+                      </p>
+                      <ul className="space-y-2">
+                        {UserSocial.map((user) => {
+                          const Icon =
+                            iconMap[user.icon as keyof typeof iconMap];
+                          return (
+                            <li
+                              key={user.name}
+                              className="flex items-center transition-all hover:text-blue-600 dark:hover:text-blue-400"
+                            >
+                              <a
+                                href={user.href}
+                                className="flex items-center "
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {Icon && <Icon className="mr-2 size-5" />}
+                                <span>{user.name}</span>
+                              </a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </CardContent>
                   </Card>
                 </section>
               </div>
